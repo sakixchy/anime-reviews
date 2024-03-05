6eb13d7cfe9d20cab882a116ee05cb33ec1d85ec
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-05 09:18:45 +0000

    register models to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from .models import Anime
 from .models import Review
+from .models import Comment
 
 # Register your models here.
 
+admin.site.register(Anime)
 admin.site.register(Review)
+admin.site.register(Comment)

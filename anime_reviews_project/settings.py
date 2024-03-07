1d8a25ce25b1b1985063e6421aa73412848baf35
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-07 18:16:57 +0000

    change settings

--- anime_reviews_project/settings.py
+++ anime_reviews_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g+b5w^v&255n-2qyd)w+%h=b4&te6xzxaobpkrugnntd45qy-m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-sakixchy-animereviews-lwlsom4sozh.ws-eu108.gitpod.io', 
 '.herokuapp.com']

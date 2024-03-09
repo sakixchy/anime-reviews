285befac5f6794384182190360a85cb1f9e31cb0
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-09 16:54:34 +0000

    serve static files to heroku

--- anime_reviews_project/settings.py
+++ anime_reviews_project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # CLOUDINARY IMPORT
 

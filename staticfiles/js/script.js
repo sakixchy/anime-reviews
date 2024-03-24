d3be7649fd754e4b0589ef8acf0a0fa11bfcada6
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-24 08:58:17 +0000

    collectstatic

---
+++ staticfiles/js/script.js
@@ -0,0 +1,10 @@
+const filterButton = document.getElementById('filterButton');
+
+
+filterButton.addEventListener('click', function() {
+
+    let modal = document.getElementById('reviewModal');
+    let bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+    });
+  

036441f0e86cb37fb3397e8fb46af72dbabd3423
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-31 23:26:29 +0000

    chanage contrast ratio of comment box

--- anime_reviews_project/settings.py
+++ anime_reviews_project/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['8000-sakixchy-animereviews-q0y8dv01pj1.ws-eu110.gitpod.io',
                  '.herokuapp.com']
 

96747d5dcf0c4375c3280fa4423152c4ec272fb2
Saki Chy <sakibchowdhury79@gmail.com>
2024-03-31 10:09:13 +0000

    correct wrong attribute for bug

--- anime_reviews_project/settings.py
+++ anime_reviews_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-sakixchy-animereviews-q0y8dv01pj1.ws-eu110.gitpod.io',
                  '.herokuapp.com']
